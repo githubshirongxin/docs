b91a6c3afde2d56b72d9253829df5de711f7e629
shirongxin <shirx@ccbjb.com.cn>
2020-07-28 06:47:16 +0000

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -6,7 +6,7 @@ const nav = require("./nav");
 module.exports = {
   base: "/docs/",
   title: 'blog',
-  dest: "public"
+  dest: "public",
   description: '思想persistent',
   head: headConf,
   markdown: {
