7a12a0269ff79918937ea7361f1e580b3a415ac9
shirongxin <shirx@ccbjb.com.cn>
2020-07-31 15:50:00 +0800

    I update my blog .md again

---
+++ lazypush.sh
@@ -0,0 +1,14 @@
+echo "git add -A"
+git add -A
+
+echo "git commit "
+git commit -m"I update my blog .md again"
+
+echo "git push origin gitlab.ccbjb.com.cn\n"
+git push origin 
+
+echo "\n git push origin gitlab.com\n"
+git push originRemote
+
+echo "\n git push origin github.com\n"
+git push originGithub
