13c5daa9ce2439a3b31d1d33461ae5b400ded2e1
shirongxin <shirx@ccbjb.com.cn>
2021-05-12 13:38:50 +0800

    I update my blog .md again

--- docs/.vuepress/nav.js
+++ docs/.vuepress/nav.js
@@ -1,151 +1,170 @@
 module.exports = [{
-    "text": "基础设施",
-    "items": [
-        { "text": "linux-powshell", "link": "/基础设施/linux-powshell/" },
-        { "text": "windows", "link": "/基础设施/windows/" },
-        { "text": "mac", "link": "/基础设施/mac/" },
-        { "text": "maven", "link": "/基础设施/maven/" },        
-        { "text": "nexus", "link": "/基础设施/nexus/" },     
-        { "text": "nexus-docker", "link": "/基础设施/nexus-docker/" },        
-        { "text": "nexus-maven", "link": "/基础设施/nexus-maven/" },        
-        { "text": "nexus-npm", "link": "/基础设施/nexus-npm/" },           
-        { "text": "vuepress", "link": "/基础设施/vuepress/" },
-        { "text": "eclipse", "link": "/基础设施/eclipse/" },
-        { "text": "docker", "link": "/基础设施/docker/" },
-        { "text": "gitlab", "link": "/基础设施/gitlab/" },
-        { "text": "mysql", "link": "/基础设施/mysql/" },
-        { "text": "高可用系统", "link": "/基础设施/高可用系统/" },
-        { "text": "自签证书", "link": "/基础设施/自签证书/" },
-        { "text": "NFS", "link": "/基础设施/NFS/" },
-        { "text": "git", "link": "/基础设施/git/" },
-        { "text": "k8s", "link": "/基础设施/k8s/" },
-        { "text": "静态代码分析", "link": "/基础设施/静态代码分析/" },
-        { "text": "基础设施", "link": "/基础设施/" }]
-},
-{
-    "text": "移动端", "items": [
-        { "text": "react-native", "link": "/移动端/跨平台/react-native/" },
-        { "text": "flutter", "link": "/移动端/跨平台/flutter/" },
-        { "text": "ios", "link": "/移动端/ios/" },
-        { "text": "跨平台", "link": "/移动端/跨平台/" },
-        { "text": "安卓", "link": "/移动端/安卓/" },
-        { "text": "移动端", "link": "/移动端/" }]
-},
-{
-    "text": "application", "items": [
-        { "text": "认证中心", "link": "/application/认证中心/" },
-        { "text": "在线教育", "link": "/application/在线教育/" },
-        { "text": "考试系统", "link": "/application/考试系统/" },
-        { "text": "单点登录", "link": "/application/单点登录/" },
-        { "text": "jira", "link": "/application/jira/" },
-        { "text": "PMO接口", "link": "/application/PMO接口/" },
-        { "text": "application", "link": "/application/" }]
-},
-{
-    "text": "前端", "items": [
-        { "text": "前端工具", "link": "/前端/前端工具/" },
-        { "text": "vue", "link": "/前端/vue/" },
-        { "text": "angular", "link": "/前端/angular/" },
-        { "text": "js", "link": "/前端/js/" },
-        { "text": "ES6", "link": "/前端/ES6/" },
-        { "text": "TS", "link": "/前端/TS/" },
-        { "text": "Rxjs", "link": "/前端/Rxjs/" },        
-        { "text": "前端", "link": "/前端/" }
-    ]
-},
-{
-    "text": "前端购买的书籍", "items": [
-        { "text": "大厂 H5 开发实战手册", "link": "/前端购买的书籍/大厂 H5 开发实战手册/" },
-        { "text": "前端性能优化管理与实践", "link": "/前端购买的书籍/前端性能优化管理与实践/" },
-        { "text": "如何使用 Canvas 制作出炫酷的网页特效", "link": "/前端购买的书籍/如何使用 Canvas 制作出炫酷的网页特效/" },
-        { "text": "深入浅出TypeScript：从基础知到类型编程", "link": "/前端购买的书籍/深入浅出TypeScript：从基础知到类型编程/" },
-        { "text": "玩转CSS的艺术之美", "link": "/前端购买的书籍/玩转CSS的艺术之美/" },
-        { "text": "SVG动画开发实战手册", "link": "/前端购买的书籍/SVG动画开发实战手册/" },
-        { "text": "Vue.js组件精讲", "link": "/前端购买的书籍/Vue.js组件精讲/" },
-        { "text": "前端购买的书籍", "link": "/前端购买的书籍/" }
-    ]
-},
+        "text": "基础设施",
+        "items": [
+            { "text": "linux-powshell", "link": "/基础设施/linux-powshell/" },
+            { "text": "windows", "link": "/基础设施/windows/" },
+            { "text": "mac", "link": "/基础设施/mac/" },
+            { "text": "maven", "link": "/基础设施/maven/" },
+            { "text": "nexus", "link": "/基础设施/nexus/" },
+            { "text": "nexus-docker", "link": "/基础设施/nexus-docker/" },
+            { "text": "nexus-maven", "link": "/基础设施/nexus-maven/" },
+            { "text": "nexus-npm", "link": "/基础设施/nexus-npm/" },
+            { "text": "vuepress", "link": "/基础设施/vuepress/" },
+            { "text": "eclipse", "link": "/基础设施/eclipse/" },
+            { "text": "docker", "link": "/基础设施/docker/" },
+            { "text": "gitlab", "link": "/基础设施/gitlab/" },
+            { "text": "mysql", "link": "/基础设施/mysql/" },
+            { "text": "高可用系统", "link": "/基础设施/高可用系统/" },
+            { "text": "自签证书", "link": "/基础设施/自签证书/" },
+            { "text": "NFS", "link": "/基础设施/NFS/" },
+            { "text": "git", "link": "/基础设施/git/" },
+            { "text": "k8s", "link": "/基础设施/k8s/" },
+            { "text": "静态代码分析", "link": "/基础设施/静态代码分析/" },
+            { "text": "基础设施", "link": "/基础设施/" }
+        ]
+    },
+    {
+        "text": "移动端",
+        "items": [
+            { "text": "react-native", "link": "/移动端/跨平台/react-native/" },
+            { "text": "flutter", "link": "/移动端/跨平台/flutter/" },
+            { "text": "ios", "link": "/移动端/ios/" },
+            { "text": "跨平台", "link": "/移动端/跨平台/" },
+            { "text": "安卓", "link": "/移动端/安卓/" },
+            { "text": "移动端", "link": "/移动端/" }
+        ]
+    },
+    {
+        "text": "application",
+        "items": [
+            { "text": "认证中心", "link": "/application/认证中心/" },
+            { "text": "在线教育", "link": "/application/在线教育/" },
+            { "text": "考试系统", "link": "/application/考试系统/" },
+            { "text": "单点登录", "link": "/application/单点登录/" },
+            { "text": "jira", "link": "/application/jira/" },
+            { "text": "PMO接口", "link": "/application/PMO接口/" },
+            { "text": "application", "link": "/application/" }
+        ]
+    },
+    {
+        "text": "前端",
+        "items": [
+            { "text": "前端工具", "link": "/前端/前端工具/" },
+            { "text": "csshtml", "link": "/前端/csshtml/" },
+            { "text": "vue", "link": "/前端/vue/" },
+            { "text": "angular", "link": "/前端/angular/" },
+            { "text": "js", "link": "/前端/js/" },
+            { "text": "ES6", "link": "/前端/ES6/" },
+            { "text": "TS", "link": "/前端/TS/" },
+            { "text": "Rxjs", "link": "/前端/Rxjs/" },
+            { "text": "前端", "link": "/前端/" }
+        ]
+    },
+    {
+        "text": "前端购买的书籍",
+        "items": [
+            { "text": "大厂 H5 开发实战手册", "link": "/前端购买的书籍/大厂 H5 开发实战手册/" },
+            { "text": "前端性能优化管理与实践", "link": "/前端购买的书籍/前端性能优化管理与实践/" },
+            { "text": "如何使用 Canvas 制作出炫酷的网页特效", "link": "/前端购买的书籍/如何使用 Canvas 制作出炫酷的网页特效/" },
+            { "text": "深入浅出TypeScript：从基础知到类型编程", "link": "/前端购买的书籍/深入浅出TypeScript：从基础知到类型编程/" },
+            { "text": "玩转CSS的艺术之美", "link": "/前端购买的书籍/玩转CSS的艺术之美/" },
+            { "text": "SVG动画开发实战手册", "link": "/前端购买的书籍/SVG动画开发实战手册/" },
+            { "text": "Vue.js组件精讲", "link": "/前端购买的书籍/Vue.js组件精讲/" },
+            { "text": "前端购买的书籍", "link": "/前端购买的书籍/" }
+        ]
+    },
 
-{
-    "text": "后端", "items": [
-        { "text": "elasticSearch", "link": "/后端/elasticSearch/" },
-        { "text": "redis", "link": "/后端/redis/" },
-        { "text": "hadoop", "link": "/后端/hadoop/" },
-        { "text": "shiro", "link": "/后端/shiro/" },
-        { "text": "golang", "link": "/后端/golang/" },
-        { "text": "kotlin", "link": "/后端/kotlin/" },
-        { "text": "nodejs", "link": "/后端/nodejs/" },
-        { "text": "python", "link": "/后端/python/" },
-        { "text": "java", "link": "/后端/java/" },
-        { "text": "junit", "link": "/后端/junit/" },
-        { "text": "springboot", "link": "/后端/springboot/" },
-        { "text": "springcloud", "link": "/后端/springcloud/" },
-        { "text": "微服务", "link": "/后端/微服务/" }
-    ]
-},
-{
-    "text": "BT", "items": [
-        { "text": "COBOL", "link": "/BT/COBOL/" }
-    ]
-},
-{
-    "text": "开发工具", "items": [
-        { "text": "授课用工具", "link": "/开发工具/授课用工具/" },
-        { "text": "cmder", "link": "/开发工具/cmder/" },
-        { "text": "爬虫", "link": "/开发工具/爬虫/" },
-        { "text": "eclipse", "link": "/开发工具/eclipse/" },
-        { "text": "IDEA", "link": "/开发工具/IDEA/" },
-        { "text": "vscode", "link": "/开发工具/vscode/" },
-        { "text": "tomcat", "link": "/开发工具/tomcat/" }
-    ]
-},
-{
-    "text": "底层研究", "items": [
-        { "text": "nginx源码分析", "link": "/底层研究/nginx源码分析/" },
-        { "text": "linux内核", "link": "/底层研究/linux内核/" },
-        { "text": "网络编程", "link": "/底层研究/网络编程/" },
-        { "text": "底层研究", "link": "/底层研究/" }
-    ]
-},
-{
-    "text": "媒体", "items": [
-        { "text": "nginx直播点播", "link": "/媒体/nginx直播点播/" }
-    ]
-},
-{
-    "text": "偏硬件5G", "items": [
-        { "text": "C++", "link": "/偏硬件5G/C++/" },
-        { "text": "汇编", "link": "/偏硬件5G/汇编/" },
-        { "text": "C", "link": "/偏硬件5G/C/" },
-        { "text": "lot", "link": "/偏硬件5G/lot/" },
-        { "text": "processing", "link": "/偏硬件5G/processing/" }
-    ]
-},
-{
-    "text": "云", "items": [
-        { "text": "GCP", "link": "/云/GCP/" }        
-    ]
-},
-{
-    "text": "微信相关", "items": [
-        { "text": "小程序", "link": "/微信相关/小程序/" },
-        { "text": "微信相关", "link": "/微信相关/" }
-    ]
-},
-{
-    "text": "webpack", "items": [
-        { "text": "webpack", "link": "/webpack/" }
-    ]
-},
-{
-    "text": "证券业务", "items": [
-        { "text": "股票", "link": "/证券业务/股票/" }
-    ]
-},
-{
-    "text": "管理", "items": [
-        { "text": "見積", "link": "/管理/見積/" },
-        { "text": "新人导入", "link": "/管理/新人导入/" }
-    ]
-}
+    {
+        "text": "后端",
+        "items": [
+            { "text": "elasticSearch", "link": "/后端/elasticSearch/" },
+            { "text": "redis", "link": "/后端/redis/" },
+            { "text": "hadoop", "link": "/后端/hadoop/" },
+            { "text": "shiro", "link": "/后端/shiro/" },
+            { "text": "golang", "link": "/后端/golang/" },
+            { "text": "kotlin", "link": "/后端/kotlin/" },
+            { "text": "nodejs", "link": "/后端/nodejs/" },
+            { "text": "python", "link": "/后端/python/" },
+            { "text": "java", "link": "/后端/java/" },
+            { "text": "junit", "link": "/后端/junit/" },
+            { "text": "springboot", "link": "/后端/springboot/" },
+            { "text": "springcloud", "link": "/后端/springcloud/" },
+            { "text": "微服务", "link": "/后端/微服务/" }
+        ]
+    },
+    {
+        "text": "BT",
+        "items": [
+            { "text": "COBOL", "link": "/BT/COBOL/" }
+        ]
+    },
+    {
+        "text": "开发工具",
+        "items": [
+            { "text": "授课用工具", "link": "/开发工具/授课用工具/" },
+            { "text": "cmder", "link": "/开发工具/cmder/" },
+            { "text": "爬虫", "link": "/开发工具/爬虫/" },
+            { "text": "eclipse", "link": "/开发工具/eclipse/" },
+            { "text": "IDEA", "link": "/开发工具/IDEA/" },
+            { "text": "vscode", "link": "/开发工具/vscode/" },
+            { "text": "tomcat", "link": "/开发工具/tomcat/" }
+        ]
+    },
+    {
+        "text": "底层研究",
+        "items": [
+            { "text": "nginx源码分析", "link": "/底层研究/nginx源码分析/" },
+            { "text": "linux内核", "link": "/底层研究/linux内核/" },
+            { "text": "网络编程", "link": "/底层研究/网络编程/" },
+            { "text": "底层研究", "link": "/底层研究/" }
+        ]
+    },
+    {
+        "text": "媒体",
+        "items": [
+            { "text": "nginx直播点播", "link": "/媒体/nginx直播点播/" }
+        ]
+    },
+    {
+        "text": "偏硬件5G",
+        "items": [
+            { "text": "C++", "link": "/偏硬件5G/C++/" },
+            { "text": "汇编", "link": "/偏硬件5G/汇编/" },
+            { "text": "C", "link": "/偏硬件5G/C/" },
+            { "text": "lot", "link": "/偏硬件5G/lot/" },
+            { "text": "processing", "link": "/偏硬件5G/processing/" }
+        ]
+    },
+    {
+        "text": "云",
+        "items": [
+            { "text": "GCP", "link": "/云/GCP/" }
+        ]
+    },
+    {
+        "text": "微信相关",
+        "items": [
+            { "text": "小程序", "link": "/微信相关/小程序/" },
+            { "text": "微信相关", "link": "/微信相关/" }
+        ]
+    },
+    {
+        "text": "webpack",
+        "items": [
+            { "text": "webpack", "link": "/webpack/" }
+        ]
+    },
+    {
+        "text": "证券业务",
+        "items": [
+            { "text": "股票", "link": "/证券业务/股票/" }
+        ]
+    },
+    {
+        "text": "管理",
+        "items": [
+            { "text": "見積", "link": "/管理/見積/" },
+            { "text": "新人导入", "link": "/管理/新人导入/" }
+        ]
+    }
 ];
